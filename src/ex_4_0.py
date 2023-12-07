890aa84fb54fdfa364c35b9794a9a314b54137e1
surya1291 <149558206+surya1291@users.noreply.github.com>
2023-12-06 21:18:25 -0600

    Update ex_4_0.py

--- src/ex_4_0.py
+++ src/ex_4_0.py
@@ -13,7 +13,13 @@ def get_shutdown_events(logfile):
     """
     Your docstring here.  Replace the pass keyword below with your implementation
     """
-    pass
+    lines = list()
+    with open(logfile, 'r') as lf:
+        logsdata = lf.read()
+    for line in logsdata.splitlines():
+        if 'Shutdown initiated' in line :
+            lines.append(line)
+    return lines
 
 
 # >>>> The code below will call your function and print the results
